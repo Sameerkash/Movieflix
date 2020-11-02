3e43ecfcfcd3faa8319824705116bb299b9c5505
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-11-02 17:07:31 +0530

    added faqs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { FooterContainer } from "./containers/footer";
 import { JumbotronContainer } from "./containers/jumbotron";
-
+import { FaqsContainer } from "./containers/faqs";
 export default function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
